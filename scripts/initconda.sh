f26d21812f0f53e7a2386a7e3addfc5af45f8a00
pratzy <mail.prateek.agarwal@gmail.com>
2020-05-30 21:46:42 +0800

    Update initconda.sh

--- scripts/initconda.sh
+++ scripts/initconda.sh
@@ -2,5 +2,5 @@
 
 # Can be installed on anaconda3 docker container or host as well.
 conda init
-conda create -y -q --name py38 conda python=3.8- psycopg2 sqlalchemy=1.3.17
+conda create -y -q --name py38 conda python=3.8 psycopg2 sqlalchemy=1.3.17
 conda activate py38
