e9e12fc83231d5e140313545b0037082cc6c94ed
Prateek Agarwal <pratzy06@gmail.com>
2020-05-31 02:12:51 +0800

    More code

--- src/python/test.py
+++ src/python/test.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.schema import MetaData
 
-sandbox_engine = create_engine('postgresql://docker:docker@postgres-10:5432/sandbox')
+sandbox_engine = create_engine(
+    'postgresql://docker:docker@postgres-10:5432/sandbox')
 meta = MetaData()
 meta.reflect(bind=sandbox_engine)
-
 
 result_set = sandbox_engine.execute("SELECT * FROM test")
 for r in result_set:
@@ -16,5 +16,5 @@ for t in meta.tables:
 
 print("Printing contents of test")
 table_test = meta.tables['test']
-for r in sandbox_engine.execute(table_test.select()): 
-  print(r) 
+for r in sandbox_engine.execute(table_test.select()):
+  print(r)
